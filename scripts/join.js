2d20bac1ad830aeb1f47b3643a256be0e586eed8
t_developer <towolabi@byupathway.edu>
2025-11-29 04:24:26 +0100

    efghj

--- scripts/join.js
+++ scripts/join.js
@@ -1,3 +1,42 @@
+
+// Generate timestamp when the form loads
+document.addEventListener("DOMContentLoaded", () => {
+    const timestampField = document.getElementById("timestamp");
+
+    // Format the timestamp (Nigeria Time)
+    const now = new Date();
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+        timeZone: "Africa/Lagos"
+    };
+
+    const formatted = now.toLocaleString("en-US", options);
+    timestampField.value = formatted;
+});
+
+// Display submitted form data on thankyou.html
+    const params = new URLSearchParams(window.location.search);
+
+    document.querySelector("#results").innerHTML = `
+    <p><strong>First Name:</strong> ${params.get("first")}</p>
+    <p><strong>Last Name:</strong> ${params.get("last")}</p>
+    <p><strong>Organizational Title:</strong> ${params.get("orgTitle")}</p>
+    <p><strong>Email:</strong> ${params.get("email")}</p>
+    <p><strong>Phone:</strong> ${params.get("phone")}</p>
+    <p><strong>Business:</strong> ${params.get("business")}</p>
+    <p><strong>Membership Level:</strong> ${params.get("membership")}</p>
+    <p><strong>Description:</strong> ${params.get("description")}</p>
+    <p><strong>Timestamp:</strong> ${params.get("timestamp")}</p>
+        `;
+
+
+
 // Set current timestamp on form load
 const timestampField = document.getElementById('timestamp');
 if (timestampField) {
